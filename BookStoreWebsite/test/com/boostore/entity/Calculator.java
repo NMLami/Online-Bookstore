77ddb921b6bb57df06409226cf1f47822af60706
NMLami <nafilmahmud@iut-dhaka.edu>
2020-12-25 15:16:17 +0600

    first commit

---
+++ BookStoreWebsite/test/com/boostore/entity/Calculator.java
@@ -0,0 +1,8 @@
+package com.boostore.entity;
+
+public class Calculator {
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+}
---
+++ BookStoreWebsite/test/com/boostore/entity/CategoryTest.java
@@ -0,0 +1,30 @@
+package com.boostore.entity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.bookstore.entity.Category;
+
+public class CategoryTest {
+
+	public static void main(String[] args) {
+
+		Category newCat = new Category();
+		newCat.setName("Horror");
+
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+		entityManager.getTransaction().begin();
+
+		entityManager.persist(newCat);
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
+
+		System.out.println(" A Categoty object was persisted");
+	}
+
+}
